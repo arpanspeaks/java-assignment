407fea2d25561a121b1261c4bae7502bfea733cc
Arpan Sardar <arpanspeaks@gmail.com>
2022-01-21 18:18:07 +0530

    Assignment 3 added

---
+++ week_1/assignment_3/Book.java
@@ -0,0 +1,23 @@
+package week_1.assignment_3;
+
+public class Book {
+	private String book_name;
+	private double book_price;
+
+	public String getBook_name() {
+		return book_name;
+	}
+
+	public void setBook_name(String book_name) {
+		this.book_name = book_name;
+	}
+
+	public double getBook_price() {
+		return book_price;
+	}
+
+	public void setBook_price(double book_price) {
+		this.book_price = book_price;
+	}
+
+}
--- week_1/assignment_3/Main.java
+++ week_1/assignment_3/Main.java
@@ -1,10 +1,51 @@
 package week_1.assignment_3;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * 
+	Create a class Book which describes its Book_title and Book_price. 
+	Use getter and setter methods to get & set the Books description.
+	Implement createBooks and showBooks methods to create objects of Display the books along with its description as follows:-
+	
+		  Book Title			Price
+		  Java Programming	    Rs.350.50
+		  Let Us C			    Rs.200.00
+		
+	Note: createBooks & showBooks should not be member functions of Book class.
+
+*/
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Book> books = new ArrayList<>();
+		books = createBooks();
+		showBooks(books);
+	}
+	
+	private static List<Book> createBooks() {
+		List<Book> books = new ArrayList<>();
+		
+		Book b1 = new Book();
+		b1.setBook_name("Java Programming");
+		b1.setBook_price(350.50);
+		books.add(b1);
+		
+		Book b2 = new Book();
+		b2.setBook_name("Let Us C");
+		b2.setBook_price(200.00);
+		books.add(b2);
+		
+		return books;
+	}
+	
+	private static void showBooks(List<Book> books) {
+		System.out.println("Book Title\t\tPrice");
+		books.forEach(book -> System.out.println(book.getBook_name() + "\t" + book.getBook_price()));
+		// todo: formatting
 	}
 
 }
